6eab3626547f803a482585b86468c6ce7688ccc3
Rodrigo Valla <rodrigovalla@protonmail.ch>
2025-05-03 16:44:37 -0300

    Almost ready to send calendars remainders by mail. Working in html formatting...

--- examples/test_mail.py
+++ examples/test_mail.py
@@ -2,43 +2,57 @@ import sys
 sys.path.append("..")
 
 import json
+import datetime as dt
 import authorize as auth
 from gmail import GMail
+from htmlformat import HtmlFormat
 
 config = json.load(open("../data/config.json")) #We load the configuration file...
-auth.update_token(config["ex_auth"], [config["mail_scope"], config["calendar_scope"]]) #First we create or update our token...
+#auth.update_token(config["ex_auth"], [config["mail_scope"], config["calendar_scope"]]) #First we create or update our token...
 
 #We need an instance of Mail():
-mail = GMail(config["ex_token"], [config["mail_scope"], config["calendar_scope"]])
+#mail = GMail(config["ex_token"], [config["mail_scope"], config["calendar_scope"]])
+html = HtmlFormat(config["ex_html"], config["file_prefix"])
 
 #We ask the user the destination email address:
 to = input()
 
+#We create some random events...
+next_week_events = []
+next_week_events.append(["Test", "Evento misterioso", "Un evento loquísimo y misterioso que da que hablar.",
+                        dt.datetime(2025,5,10,11,35), dt.datetime(2025,5,10,12,35),
+                        (dt.datetime(2025,5,10,11,35)-dt.datetime.today()).days, None])
+next_week_events.append(["Test", "Evento misterioso", "Un evento loquísimo y misterioso que da que hablar.",
+                        dt.datetime(2025,5,11,11,35), dt.datetime(2025,5,11,12,35),
+                        (dt.datetime(2025,5,12,11,35)-dt.datetime.today()).days, "https://meet.google.com/saraza"])
+next_week_events.append(["Test", "Evento misterioso", "Un evento loquísimo y misterioso que da que hablar.",
+                        dt.datetime(2025,5,15), dt.datetime(2025,5,16),
+                        (dt.datetime(2025,5,15)-dt.datetime.today()).days, None])
+
+later_events = []
+later_events.append(["Test", "Evento misterioso", "Un evento loquísimo y misterioso que da que hablar.",
+                        dt.datetime(2025,5,21), dt.datetime(2025,5,22),
+                        (dt.datetime(2025,5,21)-dt.datetime.today()).days, None])
+later_events.append(["Test", "Evento misterioso", "Un evento loquísimo y misterioso que da que hablar.",
+                        dt.datetime(2025,5,23,10,20), dt.datetime(2025,5,23,10,40),
+                        (dt.datetime(2025,5,24)-dt.datetime.today()).days, None])
+
 #We need a body for a html message:
-message_body = "<html>"
-message_body += open("../data/html/test.css").read() #We load the css header...
-message_body += "<body>"
-message_body += open("../data/html/test_header.html").read() #We write the header...
-message_body += "<tr><td colspan=3><h1>This is a test</h1></td></tr>"
-message_body += "<tr><td colspan=3><h2>A first email in html</h2></td></tr>"
-message_body += "<tr><td colspan=3><h3>We are using a table</h3></td></tr>"
-message_body += """<tr><td>X</td><td>O</td><td>X</td></tr>
-                <tr><td>X</td><td>X</td><td>0</td></tr>
-                <tr><td>O</td><td>X</td><td>O</td></tr>"""
-message_body += "<tr><td colspan=3 style='color:#cccccc;'>This message was sent by a robot.</td></tr>"
-message_body += open("../data/html/test_footer.html").read() #We write our footer...
-message_body += "</body></html>"
+message_body = html.remainders_mail_body(next_week_events, later_events)
+file = open("testing.html", "w")
+file.write(message_body)
+file.close()
 
 #We create the html message...
-html_message = mail.create_html_mail(config["mail"], to, "Test", message_body)
+#html_message = mail.create_html_mail(config["mail"], to, "Test", message_body)
 
 #We create a body for a text message:
 message_body = "Hi, how are you?\nThis an example message sent with eds_robot.\nGood luck!"
 message_body += "\n\nIMPORTANT: This message was sent by a robot."
 
 #We create the text message...
-message = mail.create_mail(config["mail"], to, "Test", message_body)
+#message = mail.create_mail(config["mail"], to, "Test", message_body)
 
 #We are ready to send our emails...
-mail.send_mail(config["mail"], to, html_message)
-mail.send_mail(config["mail"], to, message)
+#mail.send_mail(config["mail"], to, html_message)
+#mail.send_mail(config["mail"], to, message)
---
+++ htmlformat.py
@@ -0,0 +1,81 @@
+class HtmlFormat():
+  "The class to format text into html"
+
+  def __init__(self, path_prefix, file_prefix):
+    self.date_format = "%d/%m/%Y"
+    self.time_format = "%H:%Mhs"
+    self.mail_head = open(path_prefix + "html/" + file_prefix + "head.html", "r").read()
+    self.mail_style = open(path_prefix + "html/" + file_prefix + "style.css", "r").read()
+    self.mail_header = open(path_prefix + "html/" + file_prefix + "header.html", "r").read()
+    self.mail_footer = open(path_prefix + "html/" + file_prefix + "footer.html", "r").read()
+
+  def remainders_mail_body(self, next_week_events, later_events):
+    message_body = "<html>\n<head>\n"
+    message_body += self.mail_head
+    message_body += "<style>\n"
+    message_body += self.mail_style
+    message_body += "</style>\n</head>\n<body>\n<table>\n"
+    message_body += self.mail_header
+    message_body += self.title_row("La próxima semana:")
+    message_body += self.events_rows(next_week_events, True)
+    message_body += self.hr_row()
+    message_body += self.secret_row()
+    message_body += self.title_row("Más adelante:")
+    message_body += self.events_rows(later_events, False)
+    message_body += self.mail_footer
+    message_body += "</table>\n</body>\n</html>"
+    return message_body
+
+  def empty_row(self):
+    return "<tr><td colspan=4></td></tr>"
+
+  def secret_row(self):
+    return "<tr><td colspan=4></td></tr>"
+
+  def events_rows(self, events, in_next_week):
+    rows = ""
+    for e in events:
+      rows += self.hr_row()
+      rows += self.event_row(e, in_next_week)
+    return rows
+      
+  def event_row(self, event, in_next_week):
+    row = "<tr>\n"
+    row += self.date_cell(event[3], event[5], in_next_week)
+    row += self.summary_cell(event[1], event[2], event[6])
+    row += "</tr>\n"
+    return row
+  
+  def date_cell(self, date, interval, in_next_week):
+    cell = "<td align=right>" + date.strftime(self.date_format)
+    if not date.hour == 0:
+      cell += "<br>" + date.strftime(self.time_format)
+    if not in_next_week:
+      cell += "<br>" + "en " + str(interval) + " días"
+    cell += "</td>\n"
+    return cell
+
+  def summary_cell(self, summary, description, meet_call):
+    cell = "<td colspan=3>"
+    cell += self.html_tagging("h2", summary)
+    cell += description
+    if not meet_call == None:
+      cell += "<br>" + self.html_link(meet_call, "Videollamada de Google Meet")
+    cell += "</td>\n"
+    return cell
+
+  def title_row(self, text):
+    return "<tr><td colspan=4><h1>" + text + "</h1></td></tr>\n"
+
+  def hr_row(self):
+    return "<tr><td colspan=4><hr></td></tr>\n"
+
+  def html_link(self, href, text):
+    return "<a href='" + href + "'>" + text + "</a>"
+
+  def html_tagging(self, tag, text):
+    return "<" + tag + ">" + text + "</" + tag + ">" 
+  
+  #About me...
+  def __str__(self):
+    return "I am the class to format html text."
